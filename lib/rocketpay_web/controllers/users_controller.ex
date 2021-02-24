101073e2dabf7c332eddb6d5dee66c9cf3440a97
Victor Menegazzo <victor.umenegazzo@gmail.com>
2021-02-24 14:40:23 -0300

    :sparkles: feat: handle translate errors

--- lib/rocketpay_web/controllers/users_controller.ex
+++ lib/rocketpay_web/controllers/users_controller.ex
@@ -6,16 +6,10 @@ defmodule RocketpayWeb.UsersController do
   action_fallback RocketpayWeb.FallbackController
 
   def create(conn, params) do
-    params
-      |> Rocketpay.create_user()
-      |> handle_response(conn)
-  end
-
-  defp handle_response({:ok, %User{} = user}, conn) do
-    conn
+    with {:ok, %User{} = user} <- Rocketpay.create_user(params) do
+      conn
       |> put_status(:created)
       |> render("create.json", user: user)
+    end
   end
-
-  defp handle_response({:error, _result} = error, _conn), do: error
 end
